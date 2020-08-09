a8c18b58f7d59bf40ffb5acc18cf8f9b19222174
Jatin Kumar <jatinkumarg17@gmail.com>
2020-08-09 15:41:13 -0400

    Updates

---
+++ precache-manifest.06f794d15d6281ec211debfcf0a4b29e.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71eadcbe277aa0d48094e9e48b3594d2",
+    "url": "/TeslaRC/index.html"
+  },
+  {
+    "revision": "041cbcda4103303cdc89",
+    "url": "/TeslaRC/static/css/main.207e937f.chunk.css"
+  },
+  {
+    "revision": "c014049c1187d72e3e5f",
+    "url": "/TeslaRC/static/js/2.5779f7d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TeslaRC/static/js/2.5779f7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041cbcda4103303cdc89",
+    "url": "/TeslaRC/static/js/main.96c02a98.chunk.js"
+  },
+  {
+    "revision": "599c75d4d6985bb38824",
+    "url": "/TeslaRC/static/js/runtime-main.5659ad6c.js"
+  },
+  {
+    "revision": "c8ef339847339a7f374dc08857c0413b",
+    "url": "/TeslaRC/static/media/19.c8ef3398.svg"
+  },
+  {
+    "revision": "a52693ae2ceb4b6315fecaf078a0bdbe",
+    "url": "/TeslaRC/static/media/21.a52693ae.svg"
+  },
+  {
+    "revision": "7ee66eea8c5fcc82180de8776e3e37ce",
+    "url": "/TeslaRC/static/media/60.7ee66eea.svg"
+  },
+  {
+    "revision": "246b5ba5f8b8af340199831b3a4b538b",
+    "url": "/TeslaRC/static/media/60d.246b5ba5.svg"
+  },
+  {
+    "revision": "37862a62ee2074b44d66b00fb9c1ebce",
+    "url": "/TeslaRC/static/media/75.37862a62.svg"
+  },
+  {
+    "revision": "9c937cb94afc2cdccc2cc4cf06dcbfe6",
+    "url": "/TeslaRC/static/media/75d.9c937cb9.svg"
+  },
+  {
+    "revision": "28a215f09ce0146c0b0e22fe2664c536",
+    "url": "/TeslaRC/static/media/90d.28a215f0.svg"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/TeslaRC/static/media/Roboto-Regular-webfont.30799efa.eot"
+  },
+  {
+    "revision": "3e5675c89f974f7811eeaf07e2dd5ba3",
+    "url": "/TeslaRC/static/media/Roboto-Regular-webfont.3e5675c8.woff"
+  },
+  {
+    "revision": "7f1320f7ec4f6716054d88c33235d17b",
+    "url": "/TeslaRC/static/media/Roboto-Regular-webfont.7f1320f7.ttf"
+  },
+  {
+    "revision": "da61d7ef302b8bf871dd0ae796a52f33",
+    "url": "/TeslaRC/static/media/Roboto-Regular-webfont.da61d7ef.svg"
+  },
+  {
+    "revision": "c418a045056d38fac1b7c87898e1506b",
+    "url": "/TeslaRC/static/media/ac-off.c418a045.svg"
+  },
+  {
+    "revision": "4dcccadfb884623189abd5d22d7777fc",
+    "url": "/TeslaRC/static/media/ac-on.4dcccadf.svg"
+  },
+  {
+    "revision": "a56566ba32341898ea4772990a826e86",
+    "url": "/TeslaRC/static/media/down.a56566ba.svg"
+  },
+  {
+    "revision": "987170eab046c4310d198d864f2dc8d7",
+    "url": "/TeslaRC/static/media/heat-off.987170ea.svg"
+  },
+  {
+    "revision": "2c61b1a6023103f04fb44e813e4a9a7b",
+    "url": "/TeslaRC/static/media/heat-on.2c61b1a6.svg"
+  },
+  {
+    "revision": "99c8f2040d559a70d47006716713add5",
+    "url": "/TeslaRC/static/media/logo.99c8f204.svg"
+  },
+  {
+    "revision": "62ed87b41017976b4ffff063c91288cf",
+    "url": "/TeslaRC/static/media/p100d.62ed87b4.svg"
+  },
+  {
+    "revision": "ff16203f3d0aa6c8a55315d4e3e7b287",
+    "url": "/TeslaRC/static/media/tesla.ff16203f.jpg"
+  },
+  {
+    "revision": "b3fc73bf1e58ec43c8502bbbc089913c",
+    "url": "/TeslaRC/static/media/up.b3fc73bf.svg"
+  },
+  {
+    "revision": "3b975a003195f4fbb447ee79965fee91",
+    "url": "/TeslaRC/static/media/wheel-19.3b975a00.png"
+  },
+  {
+    "revision": "e787dc928af0297e0c30ebf60ca1e0df",
+    "url": "/TeslaRC/static/media/wheel-21.e787dc92.png"
+  }
+]);
